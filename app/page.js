17850ce45ed903a35aa76b38bf070e9c247eaa8e
Sunny Dhanwani <dhanwanisunny678@gmail.com>
2024-01-31 01:28:56 +0530

    Made toolbox UI

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Menu from "@/components/Menu";
+import Toolbox from "@/components/Toolbox";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Menu />
+      <Toolbox />
     </main>
   );
 }
